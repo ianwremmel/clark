5c3505905b22c87cf70be65575dac1e109749a87
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-03-20 23:47:01 -0700

    refactor(config): use rc to load config

---
+++ src/lib/config.ts
@@ -0,0 +1,23 @@
+import * as debugFactory from 'debug';
+import * as rc from 'rc';
+
+const debug = debugFactory('clark:lib:config');
+
+export interface Config {
+  scripts?: ScriptConfig
+}
+
+export interface ScriptConfig {
+  [key: string]: string
+}
+
+export function load() : Config {
+  debug('Looking for .clarkrc files');
+  const conf = rc('clark', {});
+  if (conf.configs && conf.configs.length) {
+    debug(`Found "${conf.configs.length}" .clarkrc files`);
+    return conf;
+  }
+  debug(`Did not find any .clarkrc files`);
+  return {};
+}
--- src/magic.ts
+++ src/magic.ts
@@ -1,28 +1,11 @@
-import {readFileSync} from 'fs';
-import {resolve} from 'path';
 import {Argv} from 'yargs';
+import {load} from './lib/config';
 import {execScript, gather} from './lib/packages';
 
-interface Config {
-  scripts: ScriptsConfig;
-}
-
-interface ScriptsConfig {
-  global: ScriptConfigObject;
-  package: ScriptConfigObject;
-}
-
-interface ScriptConfigObject {
-  [key: string]: string;
-}
-
 export function magic(y: Argv): Argv {
-  const clarkrcFilename = resolve(process.cwd(), '.clarkrc.json');
-  const rawrc = readFileSync(clarkrcFilename, 'utf-8');
-  const config: Config = JSON.parse(rawrc);
-
-  if (config.scripts && config.scripts.package) {
-    return Object.entries(config.scripts.package).reduce(
+  const config = load();
+  if (config.scripts && config.scripts) {
+    return Object.entries(config.scripts).reduce(
       (yy: Argv, [command, script]: [string, string]): Argv =>
         yy.command(
           command,
