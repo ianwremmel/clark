c7e957bdf18b3037a1ea8b3b2e7eedab92bf94ac
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-03-22 18:06:28 -0700

    refactor(tests): wire up chai just once

--- test/integration/lib/run.ts
+++ test/integration/lib/run.ts
@@ -1,26 +1,29 @@
 import {execSync} from 'child_process';
 import {resolve as pathResolve} from 'path';
+import {start} from 'repl';
 import yargs from 'yargs';
 
 /**
  * Run a command in the fixture directory.
  */
-export default function run(cmd: string): Promise<string> {
-  return new Promise(resolve => {
-    process.chdir(pathResolve(__dirname, '..', 'fixtures', 'monorepo'));
+export default async function run(cmd: string): Promise<string> {
+  const startDir = process.cwd();
+  process.chdir(pathResolve(__dirname, '..', 'fixtures', 'monorepo'));
 
-    // reminder: this path is relative to the fixture directory
-    const toExec = `ts-node ${pathResolve(
-      __dirname,
-      '../../../src/cli.ts',
-    )} ${cmd}`;
+  // reminder: this path is relative to the fixture directory
+  const toExec = `ts-node ${pathResolve(
+    __dirname,
+    '../../../src/cli.ts',
+  )} ${cmd}`;
 
+  try {
     // pass {stdio: 'pipe'} to prevent error output from being printed in the test
     // report.
-    resolve(
-      execSync(toExec, {stdio: 'pipe'})
-        .toString()
-        .trim(),
-    );
-  });
+    const result = execSync(toExec, {stdio: 'pipe'})
+      .toString()
+      .trim();
+    return result;
+  } finally {
+    process.chdir(startDir);
+  }
 }
