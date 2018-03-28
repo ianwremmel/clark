37f8ca318bb702c692c31f770a9b2291f8451e15
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-03-28 15:50:47 -0700

    fix(hoist): sort deps in root package
    Fixes #36

--- src/lib/packages.ts
+++ src/lib/packages.ts
@@ -144,6 +144,9 @@ export async function hoist(packageName: string): Promise<void> {
 
   delete pkg.dependencies;
   delete pkg.devDependencies;
+
+  rootPkg.dependencies = sortObject(rootPkg.dependencies);
+
   await write(packageName, pkg);
   await writeRootPackage(rootPkg);
 }
@@ -258,4 +261,30 @@ function filterEnv(env: object): object {
 
 interface EnvObject {
   [key: string]: string;
+}
+
+interface AnyObject {
+  [key: string]: any;
+}
+
+/**
+ * Sorts an object
+ * @param obj
+ */
+function sortObject(obj: object): object {
+  return Object.entries(obj)
+    .sort((left, right) => {
+      if (left[0] < right[0]) {
+        return -1;
+      }
+      if (left[0] > right[0]) {
+        return 1;
+      }
+
+      return 0;
+    })
+    .reduce<AnyObject>((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
 }
