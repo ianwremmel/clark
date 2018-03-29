6552f878d0eb351c4abf518276c0ef3d890ba7aa
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-03-29 12:08:51 -0700

    style(version): sort function definitions

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -4,6 +4,89 @@ import semver, {SemVer} from 'semver';
 const debug = debugFactory('clark:lib:version');
 
 /**
+ * Range operators
+ */
+enum RangeOperator {
+  /**
+   * Caret operator
+   */
+  Caret = '^',
+  /**
+   * Tilde operator
+   */
+  Tilde = '~',
+  /**
+   * No operator
+   */
+  Exact = '',
+}
+
+/**
+ * Removes the range operator from a version string and converts it to a SemVer
+ * @param version
+ */
+function extractExactVersion(version: string): string {
+  const exact = semver.clean(
+    version.replace(RangeOperator.Caret, '').replace(RangeOperator.Tilde, ''),
+  );
+
+  if (!exact) {
+    throw new Error(`"${version}" is not a valid semver`);
+  }
+
+  return exact;
+}
+
+/**
+ * Extracts the range modififer from a semver string
+ * @param version
+ */
+function extractRangeOperator(version: string): RangeOperator {
+  if (version.startsWith('^')) {
+    return RangeOperator.Caret;
+  }
+  if (version.startsWith('~')) {
+    return RangeOperator.Tilde;
+  }
+
+  return RangeOperator.Exact;
+}
+
+/**
+ * Determines the most permissive range operator between two version strings
+ * @param left
+ * @param right
+ */
+function extractMostPermissiveOperator(
+  left: string,
+  right: string,
+): RangeOperator {
+  const leftType = extractRangeOperator(left);
+  const rightType = extractRangeOperator(right);
+
+  if (leftType === RangeOperator.Caret || rightType === RangeOperator.Caret) {
+    return RangeOperator.Caret;
+  }
+
+  if (leftType === RangeOperator.Tilde || rightType === RangeOperator.Tilde) {
+    return RangeOperator.Tilde;
+  }
+
+  return RangeOperator.Exact;
+}
+
+/**
+ * Indicates if two version strings have the same range operator
+ * @param left
+ * @param right
+ */
+function hasSameOperator(left: string, right: string): boolean {
+  const leftType = extractRangeOperator(left);
+  const rightType = extractRangeOperator(right);
+  return leftType === rightType;
+}
+
+/**
  * Selects the greater of two semver ranges combined with their most permissive
  * range operator
  * @param left
@@ -55,87 +138,4 @@ export function select(left: string | null, right: string | null): string {
   return `${operator}${
     semver.gt(leftExact, rightExact) ? leftExact : rightExact
   }`;
-}
-
-/**
- * Range operators
- */
-enum RangeOperator {
-  /**
-   * Caret operator
-   */
-  Caret = '^',
-  /**
-   * Tilde operator
-   */
-  Tilde = '~',
-  /**
-   * No operator
-   */
-  Exact = '',
-}
-
-/**
- * Removes the range operator from a version string and converts it to a SemVer
- * @param version
- */
-function extractExactVersion(version: string): string {
-  const exact = semver.clean(
-    version.replace(RangeOperator.Caret, '').replace(RangeOperator.Tilde, ''),
-  );
-
-  if (!exact) {
-    throw new Error(`"${version}" is not a valid semver`);
-  }
-
-  return exact;
-}
-
-/**
- * Extracts the range modififer from a semver string
- * @param version
- */
-function extractRangeOperator(version: string): RangeOperator {
-  if (version.startsWith('^')) {
-    return RangeOperator.Caret;
-  }
-  if (version.startsWith('~')) {
-    return RangeOperator.Tilde;
-  }
-
-  return RangeOperator.Exact;
-}
-
-/**
- * Indicates if two version strings have the same range operator
- * @param left
- * @param right
- */
-function hasSameOperator(left: string, right: string): boolean {
-  const leftType = extractRangeOperator(left);
-  const rightType = extractRangeOperator(right);
-  return leftType === rightType;
-}
-
-/**
- * Determines the most permissive range operator between two version strings
- * @param left
- * @param right
- */
-function extractMostPermissiveOperator(
-  left: string,
-  right: string,
-): RangeOperator {
-  const leftType = extractRangeOperator(left);
-  const rightType = extractRangeOperator(right);
-
-  if (leftType === RangeOperator.Caret || rightType === RangeOperator.Caret) {
-    return RangeOperator.Caret;
-  }
-
-  if (leftType === RangeOperator.Tilde || rightType === RangeOperator.Tilde) {
-    return RangeOperator.Tilde;
-  }
-
-  return RangeOperator.Exact;
 }
