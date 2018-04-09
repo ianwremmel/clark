9c0efea579e42f96e802cf72193731972160ac4e
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-04-09 14:02:01 -0700

    build(eslint): add prettier to eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,13 +2,13 @@
 
 // @commitlint/config-angular changed the rules around valid scope names and
 // Greenkeeper still uses the old format.
-if (process.env.CIRCLE_BRANCH && process.env.CIRCLE_BRANCH.includes('greenkeeper')) {
+if (
+  process.env.CIRCLE_BRANCH &&
+  process.env.CIRCLE_BRANCH.includes('greenkeeper')
+) {
   module.exports = {};
-}
-else {
+} else {
   module.exports = {
-    extends: [
-      '@commitlint/config-angular'
-    ]
+    extends: ['@commitlint/config-angular'],
   };
 }
