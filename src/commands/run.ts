3fa54c6432c11caaabdbcd6f83d1e9f756335dad
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-04-11 22:11:29 -0700

    docs(run): update docs for newish run behavior

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -5,7 +5,7 @@ const RunCommand: CommandModule = {
   command: 'run [script]',
 
   describe:
-    'Runs a script in each package directory. This is different from `exec` in that scripts must be defined in .clarkrc and may be overridden on a per-package basis via npm scripts.',
+    'Runs a script in each package directory. This is different from `exec` in that scripts should be defined in .clarkrc and may be overridden on a per-package basis via npm scripts. npm scripts defined only in subpackage package.jsons can be run this way, but only scripts named in .clarkrc will populate the help output.',
 
   builder: Run.builder,
 
