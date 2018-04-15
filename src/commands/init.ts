47abe5acf08cdb92d12e5c136f31daa19a292549
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-04-15 09:47:57 -0700

    refactor(init): migrate init to oclif

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,29 +1,66 @@
-import {CommandModule} from 'yargs';
+import {Command, flags} from '@oclif/command';
+import {writeFile} from 'mz/fs';
+import {resolve} from 'path';
 
-import {Init} from '../lib/handlers/init';
+import {Config, ScriptConfig} from '../lib/config';
+import {format as f, makeDebug} from '../lib/debug';
+import {findProjectRoot, hasRc} from '../lib/project';
 
-const InitCommand: CommandModule = {
-  describe: 'Create a .clarkrc file in your project root',
+const debug = makeDebug(__dirname);
 
-  builder(yargs) {
-    return yargs
-      .options({
-        force: {
-          alias: 'f',
-          default: false,
-          description: 'Overwrite .clarkrc with new config',
-          type: 'boolean',
-        },
-        script: {
-          alias: 's',
-          description: 'Identifies a script to add to the config file',
-          type: 'string',
-        },
-      })
-      .strict();
-  },
+export default class Init extends Command {
+  static description = 'Create a .clarkrc file in your project root';
 
-  handler: Init.handler,
-};
+  static flags = {
+    force: flags.boolean({
+      char: 'f',
+      description: 'Overwrite .clarkrc with new config',
+    }),
+    script: flags.string({
+      char: 's',
+      description: 'Identifies a script to add to the config file',
+      multiple: true,
+    }),
+  };
 
-export = InitCommand;
+  async run() {
+    const {flags: options} = this.parse(Init);
+
+    debug('locating root directory');
+    const rootDir = await findProjectRoot();
+    debug('checking if root dir contains a .clarkrc');
+
+    if ((await hasRc(rootDir)) && !options.force) {
+      throw new Error(
+        'Project already configured for clark. Pass --force to overwrite',
+      );
+    }
+
+    const extract = (s: ScriptConfig, item: string) => {
+      const [key, value] = item.split(/=(.+)/);
+      s[key] = value;
+      return s;
+    };
+
+    debug('generating scripts hash');
+    let scripts: ScriptConfig = {};
+    if (options.script) {
+      if (Array.isArray(options.script)) {
+        scripts = options.script.reduce(extract, scripts);
+      } else {
+        extract(scripts, options.script);
+      }
+    }
+
+    const config: Config = {
+      scripts,
+    };
+
+    debug(f`writing .clarkrc to ${rootDir}`);
+    writeFile(
+      resolve(rootDir, '.clarkrc'),
+      `${JSON.stringify(config, null, 2)}\n`,
+    );
+    debug(f`wrote .clarkrc to ${rootDir}`);
+  }
+}
