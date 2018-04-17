642490c2c39505143b719f294b8209b7f7451941
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-04-17 16:44:16 -0700

    refactor(run): migrate run to oclif

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,15 +1,78 @@
-import {CommandModule} from 'yargs';
-import {Run} from '../lib/handlers/run';
+import {Command, flags} from '@oclif/command';
 
-const RunCommand: CommandModule = {
-  command: 'run [script]',
+import {load} from '../lib/config';
+import {format as f} from '../lib/debug';
+import {apply, execScript} from '../lib/packages';
+import {camelizeObject} from '../lib/util';
 
-  describe:
-    'Runs a script in each package directory. This is different from `exec` in that scripts should be defined in .clarkrc and may be overridden on a per-package basis via npm scripts. npm scripts defined only in subpackage package.jsons can be run this way, but only scripts named in .clarkrc will populate the help output.',
+export default class Run extends Command {
+  static description = 'Runs a script in each package directory. This is different from `exec` in that scripts should be defined in .clarkrc and may be overridden on a per-package basis via npm scripts. npm scripts defined only in subpackage package.jsons can be run this way, but only scripts named in .clarkrc will populate the help output.';
 
-  builder: Run.builder,
+  static flags = {
+    'fail-fast': flags.boolean({
+      description:
+        'Fail as soon as a command fails, rather than running all to completion',
+    }),
+    'package-name': flags.string({
+      char: 'p',
+      description:
+        'The package against which to run this command. May be specified more than once.',
+      multiple: true,
+    }),
+    silent: flags.boolean({
+      char: 's',
+      description: 'Indicates nothing should be printed to the stdout',
+    }),
+  };
 
-  handler: Run.handler,
-};
+  static args = [{name: 'script', required: true}];
 
-export = RunCommand;
+  static strict = false;
+
+  async run() {
+    const {flags, args, argv} = this.parse(Run);
+
+    const options = {
+      ...camelizeObject(flags),
+      ...camelizeObject(args),
+    } as apply.InvocationOptions;
+
+    const script = options.script;
+    const extra = argv.join(' ').replace(script, '');
+
+    const config = load();
+    const fallbackScript = config.scripts[script];
+
+    // console.log('options:', options);
+    // console.log('scriptName:', script);
+    // console.log('extra:', extra);
+
+    await apply(
+      {
+        before: (packages) =>
+          f`Running ${script} against ${packages.length} packages`,
+        beforeEach: (packageName) =>
+          f`Running ${script} against ${packageName}`,
+        afterEach: (packageName, error) => {
+          if (error) {
+            return `${script} failed against ${packageName}`;
+          }
+          return `Ran ${script} against ${packageName}`;
+        },
+        after: (packages, errors) => {
+          if (errors.length) {
+            return f`clark run failed to execute the following command against ${
+              errors.length
+            } packages\n> ${script}\n`;
+          }
+
+          return `Ran ${script} successfully against ${packages.length}`;
+        },
+      },
+      async (packageName) => {
+        await execScript(script, packageName, fallbackScript);
+      },
+      options,
+    );
+  }
+}
--- test/integration/spec/run.ts
+++ test/integration/spec/run.ts
@@ -1,79 +1,152 @@
+import {test} from '@oclif/test';
 import {assert} from 'chai';
-
-import run from '../lib/run';
+import {resolve} from 'path';
 
 describe('run', () => {
-  it('generates commands from the local .clarkrc', async () => {
-    const result = await run('clark run --help');
-    assert.include(result, 'local');
-    assert.include(result, 'override');
-  });
+  test
+    .skip()
+    .do(() => process.chdir(resolve(__dirname, '../fixtures/monorepo')))
+    .stdout()
+    .stderr()
+    .command(['run', '--help'])
+    .it('generates commands from the local .clarkrc', async (ctx) => {
+      assert.include(ctx.stdout, 'local');
+      assert.include(ctx.stdout, 'override');
+    });
 
-  it('executes a script in each package directory', async () => {
-    const result = await run('clark run local --silent');
-    assert.equal(result, 'run\nrun\nrun');
-  });
+  test
+    .do(() => process.chdir(resolve(__dirname, '../fixtures/monorepo')))
+    .stdout()
+    .stderr()
+    .command(['run', 'local', '--silent'])
+    .it('executes a script in each package directory', async (ctx) => {
+      assert.equal(ctx.stdout, 'run\n\nrun\n\nrun\n\n');
+    });
 
   describe('when a package has an npm script of the same name', () => {
-    it('executes the override or falls back to the .clarkrc version', async () => {
-      const result = await run('clark run override --silent');
-      assert.equal(
-        result,
-        'not overridden\nnot overridden\nthis is an override',
+    test
+      .do(() => process.chdir(resolve(__dirname, '../fixtures/monorepo')))
+      .stdout()
+      .stderr()
+      .command(['run', 'override', '--silent'])
+      .it(
+        'executes the override or falls back to the .clarkrc version',
+        async (ctx) => {
+          assert.equal(
+            ctx.stdout,
+            'not overridden\n\nnot overridden\n\nthis is an override\n\n',
+          );
+        },
       );
-    });
   });
 
   describe('when invoked with --package', () => {
-    it('invokes within only that package', async () => {
-      const result = await run(
-        'clark run override --silent --package not-scoped',
-      );
-      assert.equal(result, 'this is an override');
-    });
+    test
+      .do(() => process.chdir(resolve(__dirname, '../fixtures/monorepo')))
+      .stdout()
+      .stderr()
+      .command(['run', 'override', '--silent', '--package-name', 'not-scoped'])
+      .it('invokes within only that package', async (ctx) => {
+        assert.equal(ctx.stdout, 'this is an override\n\n');
+      });
   });
 
   describe('when invoked with --package twice', () => {
-    it('invokes within only those packages', async () => {
-      const result = await run(
-        'clark run override --silent --package not-scoped --package @example/scoped-package-the-first',
+    test
+      .do(() => process.chdir(resolve(__dirname, '../fixtures/monorepo')))
+      .stdout()
+      .stderr()
+      .command([
+        'run',
+        'override',
+        '--silent',
+        '--package-name',
+        'not-scoped',
+        '--package-name',
+        '@example/scoped-package-the-first',
+      ])
+      .it('invokes within only those packages', async (ctx) => {
+        assert.equal(ctx.stdout, 'not overridden\n\nthis is an override\n\n');
+      });
+  });
+
+  test
+    .do(() =>
+      process.chdir(resolve(__dirname, '../fixtures/monorepo/packages')),
+    )
+    .stdout()
+    .stderr()
+    .command(['run', 'override', '--silent'])
+    .it('can be run from anywhere in the repo', async (ctx) => {
+      assert.equal(
+        ctx.stdout,
+        'not overridden\n\nnot overridden\n\nthis is an override\n\n',
       );
-      assert.equal(result, 'not overridden\nthis is an override');
     });
-  });
-
-  it('can be run from anywhere in the repo', async () => {
-    let result = await run('clark run override --silent', 'monorepo/packages');
-    assert.equal(result, 'not overridden\nnot overridden\nthis is an override');
-
-    result = await run(
-      'clark run override --silent --package @example/scoped-package-the-first',
-      'monorepo/packages/node_modules/not-scoped',
-    );
-    assert.equal(result, 'not overridden');
-  });
 
   describe('when run from within a package directory', () => {
-    it('infers the package name from the directory', async () => {
-      const result = await run(
-        'clark run override --silent',
-        'monorepo/packages/node_modules/not-scoped',
-      );
-      assert.equal(result, 'this is an override');
-    });
+    test
+      .do(() =>
+        process.chdir(
+          resolve(
+            __dirname,
+            '../fixtures/monorepo/packages/node_modules/not-scoped',
+          ),
+        ),
+      )
+      .stdout()
+      .stderr()
+      .command(['run', 'override', '--silent'])
+      .it('infers the package name from the directory', async (ctx) => {
+        assert.equal(ctx.stdout, 'this is an override\n\n');
+      });
+
+    test
+      .do(() =>
+        process.chdir(
+          resolve(
+            __dirname,
+            '../fixtures/monorepo/packages/node_modules/not-scoped',
+          ),
+        ),
+      )
+      .stdout()
+      .stderr()
+      .command([
+        'run',
+        'override',
+        '--silent',
+        '--package-name',
+        '@example/scoped-package-the-first',
+      ])
+      .it('prefers the packages switch to inferrence', async (ctx) => {
+        assert.equal(ctx.stdout, 'not overridden\n\n');
+      });
   });
 
   describe('when invoked with a script not in .clarkrc', () => {
-    it('runs that script in any package that defines it', async () => {
-      const result = await run('clark run --silent not-in-clarkrc');
-      assert.equal(result, '1\n2');
-    });
+    test
+      .do(() => process.chdir(resolve(__dirname, '../fixtures/monorepo')))
+      .stdout()
+      .stderr()
+      .command(['run', '--silent', 'not-in-clarkrc'])
+      .it('runs that script in any package that defines it', async (ctx) => {
+        assert.equal(ctx.stdout, '1\n\n2\n\n');
+      });
 
-    it('supports --package', async () => {
-      const result = await run(
-        'clark run --silent --package @example/scoped-package-the-first not-in-clarkrc',
-      );
-      assert.equal(result, '1');
-    });
+    test
+      .do(() => process.chdir(resolve(__dirname, '../fixtures/monorepo')))
+      .stdout()
+      .stderr()
+      .command([
+        'run',
+        '--silent',
+        '--package-name',
+        '@example/scoped-package-the-first',
+        'not-in-clarkrc',
+      ])
+      .it('supports --package', async (ctx) => {
+        assert.equal(ctx.stdout, '1\n\n');
+      });
   });
 });
